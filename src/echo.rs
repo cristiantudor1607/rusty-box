e33d6d0c9cfdcc85b93099f20a0b33cd7e45203a
Andrei Tudor <118179586+cristiantudor1607@users.noreply.github.com>
2023-10-29 20:53:10 +0000

    Fixed echo to work without arguments

--- src/echo.rs
+++ src/echo.rs
@@ -6,16 +6,35 @@ pub fn add_newline(args: &Vec<String>) -> bool {
        args[1] should be the "echo" command name
        args[2] can be "-n" option */
 
+    /* If the user type just "echo" */
+    if args.len() == 2 {
+        return true;
+    };
+
     if args[2].eq(&String::from("-n")) {
         return false;
-    }
+    };
 
     return true;
 }
 
 pub fn echo(args: &Vec<String>) -> Result<(), std::io::Error> {
-    let items: Vec<String>;
     let newln = add_newline(args);
+    let n = args.len();
+
+    /* If the user type just "echo" */
+    if n == 2 {
+        println!("");
+        return Ok(());
+    };
+
+    /* If the user type just "echo -n" */
+    if n == 3 && !newln {
+        return Ok(());
+    };
+    
+    let items: Vec<String>;
+   
 
     if newln == true {
         items = utils::extract_params_inrange(&args, 2, usize::MAX);
@@ -24,27 +43,31 @@ pub fn echo(args: &Vec<String>) -> Result<(), std::io::Error> {
     };
 
     /* Method to print inspired by https://doc.rust-lang.org/std/macro.print.html */
-    let n = items.len() - 1;
-    let mut i = 0;
     let mut lock = stdout().lock();
 
-    for item in items {
-        /* If the item isn't the last element, it should print a space */
-        if i != n {
-            match write!(lock, "{} ", item) {
-                Ok(_) => (),
-                Err(e) => return Err(e),
-            };
-        /* If the item is the last element it shouldn't print a space after */
-        } else {
-            match write!(lock, "{}", item) {
-                Ok(_) => (),
-                Err(e) => return Err(e),
-            };
-        }
+    /* I want to use write! macro, and I need to add an extra space before
+    the string, but the first one */
+    let first = &items[0];
+    match write!(lock, "{}", first) {
+        Ok(_) => (),
+        Err(e) => return Err(e),
+    };
 
-        i = i + 1;
+    /* I need a classic iterator to skip the first element in the for loop */
+    let mut i = 0;
+
+    for item in items {
+        if i == 0 {
+            i += 1;
+            continue;
+        };
+
+        match write!(lock, " {}", item) {
+            Ok(_) => (),
+            Err(e) => return Err(e),
+        };
     }
+
 
     if newln {
         print!("\n");
